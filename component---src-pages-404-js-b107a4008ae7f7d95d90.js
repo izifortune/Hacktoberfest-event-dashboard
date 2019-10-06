a02cf0c93680890c21d83a59f8330b2ecb00f70b
Fabrizio Fortunato <fortunatof@ryanair.com>
2019-10-06 21:15:07 +0200

    Updates

---
+++ component---src-pages-404-js-b107a4008ae7f7d95d90.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{213:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(211),o=n(212);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b107a4008ae7f7d95d90.js.map
