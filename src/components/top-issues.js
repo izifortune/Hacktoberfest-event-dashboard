5b65d6a99dc4790607da2e5bbd24bbebafed1fe7
Hugo <hugo@chataigner.com>
2019-10-19 12:10:41 +0100

    Add a limit on the number of issues displayed

--- src/components/top-issues.js
+++ src/components/top-issues.js
@@ -2,24 +2,30 @@ import React from "react";
 import { Card } from "./card";
 import { H3 } from "./headings";
 
+const MAX_DISPLAY_ISSUES = 10;
+let listCount = 0;
+
 // TODO Empty state
-const TopIssues = ({ issues }) => (
-  <Card>
+const TopIssues = ({ issues }) => <Card>
     <H3>Unassigned Issues</H3>
     <p>use hacktoberfestDublin label</p>
     <ul>
-      {issues
-        .filter(el => el.assignee === null)
-        .filter(el => el.state !== 'closed')
-        .map((value, index) => {
-          return (
-            <li key={index}>
-              <a href={value.html_url}>{value.title}</a>
-            </li>
-          );
-        })}
+        {
+            issues
+                .filter(el => el.assignee === null)
+                .filter(el => el.state !== 'closed')
+                .filter(() => {
+                    listCount ++;
+                    return listCount <= MAX_DISPLAY_ISSUES;
+                })
+                .map((value, index) => {
+                    return (
+                        <li key={index}>
+                            <a href={value.html_url}>{value.title}</a>
+                        </li>
+                    );
+                })}
     </ul>
-  </Card>
-);
+</Card>;
 
 export default TopIssues;
