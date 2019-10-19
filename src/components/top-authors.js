51587807061b1dc19ca5019fdaf2630d066f27f1
crisnreis <crisnreis@gmail.com>
2019-10-19 16:30:25 +0100

    Added a default empty state for issues & authors

--- src/components/top-authors.js
+++ src/components/top-authors.js
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
-import { Card } from "./card";
-import { H3 } from "./headings";
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from './card';
+import { H3 } from './headings';
 
 const Img = styled.img`
   width: 40px;
@@ -10,25 +10,36 @@ const Img = styled.img`
   margin-right: 16px;
 `;
 
-const TopAuthors = ({ pull_requests }) => (
-  <Card>
-    <H3>Top Authors</H3>
-    {pull_requests.map((value, index) => {
-      // TODO group by user login
-      // TODO Limit top 5 authors
-      const img = value.user.avatar_url;
-      return (
-        <a
-          key={index}
-          style={{ display: "flex", alignItems: "center" }}
-          href={value.user.html_url}
-        >
-          <Img src={img} />
-          {value.user.login}
-        </a>
-      );
-    })}
-  </Card>
-);
+const TopAuthors = ({ pull_requests }) => {
+  if (pull_requests != []) {
+    return (
+      <Card>
+        <H3>Top Authors</H3>
+        {pull_requests.map((value, index) => {
+          // TODO group by user login
+          // TODO Limit top 5 authors
+          const img = value.user.avatar_url;
+          return (
+            <a
+              key={index}
+              style={{ display: 'flex', alignItems: 'center' }}
+              href={value.user.html_url}
+            >
+              <Img src={img} />
+              {value.user.login}
+            </a>
+          );
+        })}
+      </Card>
+    );
+  } else {
+    return (
+      <Card>
+        <H3>Top Authors</H3>
+        <p>No authors contributed to this project so far.</p>
+      </Card>
+    );
+  }
+};
 
 export default TopAuthors;
--- src/components/top-issues.js
+++ src/components/top-issues.js
@@ -1,25 +1,36 @@
-import React from "react";
-import { Card } from "./card";
-import { H3 } from "./headings";
+import React from 'react';
+import { Card } from './card';
+import { H3 } from './headings';
 
 // TODO Empty state
-const TopIssues = ({ issues }) => (
-  <Card>
-    <H3>Unassigned Issues</H3>
-    <p>use hacktoberfestDublin label</p>
-    <ul>
-      {issues
-        .filter(el => el.assignee === null)
-        .filter(el => el.state !== 'closed')
-        .map((value, index) => {
-          return (
-            <li key={index}>
-              <a href={value.html_url}>{value.title}</a>
-            </li>
-          );
-        })}
-    </ul>
-  </Card>
-);
+const TopIssues = ({ issues }) => {
+  if (issues != []) {
+    return (
+      <Card>
+        <H3>Unassigned Issues</H3>
+        <p>use hacktoberfestDublin label</p>
+        <ul>
+          {issues
+            .filter(el => el.assignee === null)
+            .filter(el => el.state !== 'closed')
+            .map((value, index) => {
+              return (
+                <li key={index}>
+                  <a href={value.html_url}>{value.title}</a>
+                </li>
+              );
+            })}
+        </ul>
+      </Card>
+    );
+  } else {
+    return (
+      <Card>
+        <H3>Unassigned Issues</H3>
+        <p>No issues in this project :)</p>
+      </Card>
+    );
+  }
+};
 
 export default TopIssues;
